36c10a7a3f2a92d3d9829d2cce5c89fd60e3f42a
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-09 19:10:36 +0000

    Add Friendships controller

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,19 @@
+class FriendshipsController < ApplicationController
+	def create
+		@friendship = current_user.friendships.build(friend_id: params[:friend_id])
+		if @friendship.save
+			flash[:notice] = 'Added  friend'
+			redirect_to users_path
+		else
+			flash[:alert] = 'Unable to friend'
+			redirect_to root_path
+		end
+	end
+
+	def destroy
+		@friendship = current_user.friendships.find(params[:id])
+		@friendship.delete
+		flash[:notice] = 'Removed  friend'
+		redirect_to current_user
+	end
+end
