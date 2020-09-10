00e7373c331a2f6a4c4f945438c2be3eace257ef
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-10 15:35:22 +0000

    Modify User model associations with Friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :friendships
-  has_many :friends, through: :friendships
+  has_many :friends, -> { where('status = ?', 'accepted') }, through: :friendships
 
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
-  has_many :inverse_friends, through: :inverse_friendships, source: :user
+  has_many :requested_friends, -> { where('status = ?', 'requested') }, through: :friendships, source: :friend
+  has_many :pending_friends, -> { where('status = ?', 'pending') }, through: :friendships, source: :friend
 end
