b7af390c924fd4c8d091b776dc06fcd8c5726dd4
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-10 15:30:39 +0000

    Add status column to friendships table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_122713) do
+ActiveRecord::Schema.define(version: 2020_09_10_055208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_122713) do
     t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "likes", force: :cascade do |t|
