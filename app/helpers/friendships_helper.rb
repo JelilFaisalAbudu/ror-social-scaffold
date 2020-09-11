4359d751842c6fbb345edd863720ebb04c9555f9
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-11 16:53:49 +0000

    Add friendships helper methods

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,16 @@
 module FriendshipsHelper
+  def ask_friendship_or(user, friend)
+    friendship = Friendship.find_by_user_id_and_friend_id(user, friend)
+    return if !friendship.nil? || user != current_user
+
+    content_tag(:button, class: 'btn') do
+      link_to 'Add Friend', friendships_path(user_id: user, friend_id: friend), method: :post
+    end
+  end
+
+  private
+
+  def not_friends?(user, friend)
+    Friendship.find_by_user_id_and_friend_id(user, friend).nil?
+  end
 end
