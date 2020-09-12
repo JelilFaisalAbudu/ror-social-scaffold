caab984df2e3afdcee2cf139af14d382c922cac2
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-12 19:51:51 +0000

    Update FriendHelper

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,9 +1,9 @@
 module FriendshipsHelper
   def ask_friendship_or(user, friend)
     friendship = Friendship.find_by_user_id_and_friend_id(user, friend)
-    return if !friendship.nil? || user != current_user
+    return if !friendship.nil? || user == friend
 
-    content_tag(:button, class: 'btn') do
+    content_tag(:button, class: 'btn btn-primary') do
       link_to 'Add Friend', friendships_path(user_id: user, friend_id: friend), method: :post
     end
   end
