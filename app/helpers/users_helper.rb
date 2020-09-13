bafb2d5f1a134e31074a1c95362dc83b284cf5aa
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-13 17:38:14 +0000

    Add helper method to show and also delete friends

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,22 @@
 module UsersHelper
   def timeline_posts(post)
     return unless current_user.friends.include?(post.user)
-  end
+	end
+	
+	def friends
+		render 'friends'
+	end
 
-  def current_user?(user)
-    current_user == user
-  end
+	def delete_friend(friend)
+		if current_user?
+			link_to 'Delete', friendship_path(user_id: current_user, friend_id: friend), method: :delete
+		end
+	end
+	
+
+	private
+
+	def current_user?
+		@user == current_user
+	end
 end
